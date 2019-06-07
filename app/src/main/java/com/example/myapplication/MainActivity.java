ef216b1407375963700be247f840608b44c9b4ea
bell333 <siriand888@gmail.com>
2019-06-06 23:56:30 -0400

    done

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,46 +1,20 @@
 package com.example.myapplication;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -114,9 +88,9 @@ public class MainActivity extends AppCompatActivity {
                     Elements mElementBlogTitle = mBlogDocument.select("h2[class=entry-title]").select("a").eq(i);
                     String mBlogTitle = mElementBlogTitle.text();
 
-                    mAuthorNameList.add(mAuthorName);
-                    mBlogUploadDateList.add(mBlogUploadDate);
-                    mBlogTitleList.add(mBlogTitle);
+                    cNameList.add(mAuthorName);
+                    cUploadDateList.add(mBlogUploadDate);
+                    cTitleList.add(mBlogTitle);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -130,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
             RecyclerView mRecyclerView = (RecyclerView)findViewById(R.id.act_recyclerview);
 
-            DataAdapter mDataAdapter = new DataAdapter(MainActivity.this, mBlogTitleList, mAuthorNameList, mBlogUploadDateList);
+            DataAdapter mDataAdapter = new DataAdapter(MainActivity.this, cTitleList, cNameList, cUploadDateList);
             RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
             mRecyclerView.setLayoutManager(mLayoutManager);
             mRecyclerView.setAdapter(mDataAdapter);
@@ -139,239 +113,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-   /* private class GetContacts extends AsyncTask<Void, Void, Void> {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // Showing progress dialog
-            pDialog = new ProgressDialog(MainActivity.this);
-            pDialog.setMessage("Please wait...");
-            pDialog.setCancelable(false);
-            pDialog.show();
-
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            HttpHandler sh = new HttpHandler();
-
-            // Making a request to url and getting response
-            String jsonStr = sh.makeServiceCall(url);
-
-            Log.e(TAG, "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb " + jsonStr);
-
-            if (jsonStr != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
-
-                    // Getting JSON Array node
-                    JSONArray contacts = jsonObj.getJSONArray("contacts");
-
-                    // looping through All Contacts
-                    for (int i = 0; i < contacts.length(); i++) {
-                        JSONObject c = contacts.getJSONObject(i);
-
-                        String id = c.getString("commits");
-                        Log.e(TAG, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa " + id);
-                        //String name = c.getString("content");
-                        //String email = c.getString("description");
-                      //  String address = c.getString("address");
-                        //String gender = c.getString("gender");
-
-                        // Phone node is JSON Object
-                        *//*JSONObject phone = c.getJSONObject("phone");
-                        String mobile = phone.getString("mobile");
-                        String home = phone.getString("home");
-                        String office = phone.getString("office");
-*//*
-                        // tmp hash map for single contact
-                        HashMap<String, String> contact = new HashMap<>();
-
-                        // adding each child node to HashMap key => value
-                        contact.put("name", id);
-                        //contact.put("content", name);
-                        //contact.put("description", email);
-                        //contact.put("mobile", mobile);
-
-                        // adding contact to contact list
-                        contactList.add(contact);
-                    }
-                } catch (final JSONException e) {
-                    Log.e(TAG, "Json parsing error: " + e.getMessage());
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "Json parsing error: " + e.getMessage(),
-                                    Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    });
-
-                }
-            } else {
-                Log.e(TAG, "Couldn't get json from server.");
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),
-                                "Couldn't get json from server. Check LogCat for possible errors!",
-                                Toast.LENGTH_LONG)
-                                .show();
-                    }
-                });
-
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            // Dismiss the progress dialog
-            if (pDialog.isShowing())
-                pDialog.dismiss();
-            *//**
-             * Updating parsed JSON data into ListView
-             * *//*
-            ListAdapter adapter = new SimpleAdapter(
-                    MainActivity.this, contactList,
-                    R.layout.list_item, new String[]{"name", "email",
-                    "mobile"}, new int[]{R.id.name,
-                    R.id.email, R.id.mobile});
-
-            lv.setAdapter(adapter);
-        }
-
-    }*/
 }
-        /*FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        }
-        );*/
-       /* WebView webView = (WebView)findViewById(R.id.webView);
-//you can load an html code
-        webView.loadData("yourCode Html to load on the webView " , "text/html" , "utf-8");
-// you can load an URL
-        webView.loadUrl("https://github.com/atapata/MyApplication/commits/master");
-        Button cbutton1 = (Button) findViewById(R.id.cbutton1);
-*/
-       /* cbutton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new yourDataTask().execute();
-            }
-        });*/
 
-
-        //  execute("Url address here");
-      /*  Authenticator.setDefault(new Authenticator() {
-                           @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("username", "password".toCharArray());
-                }
-            });
-            HttpURLConnection urlConnection;
-            URL url = null;
-            InputStream inputStream;
-
-
-        try {
-            url = new URL("https://github.com/atapata/MyApplication/commits/master" + ctext);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        System.out.println(url);
-*/
-
-             //   urlConnection = (HttpURLConnection) url.openConnection();
-
-
-
-
-
-
-   /* protected class yourDataTask extends AsyncTask <Void, Void, JSONObject>
-    {
-        @Override
-        protected JSONObject doInBackground(Void... params)
-        {
-
-            String str="https://github.com/atapata/MyApplication/commits/master";
-            URLConnection urlConn = null;
-            BufferedReader bufferedReader = null;
-            try
-            {
-                URL url = new URL(str);
-                urlConn = url.openConnection();
-                bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
-
-                StringBuffer stringBuffer = new StringBuffer();
-                String line;
-                while ((line = bufferedReader.readLine()) != null)
-                {
-                    stringBuffer.append(line);
-                }
-
-                return new JSONObject(stringBuffer.toString());
-            }
-            catch(Exception ex)
-            {
-                Log.e("App", "yourDataTask", ex);
-                return null;
-            }
-            finally
-            {
-                if(bufferedReader != null)
-                {
-                    try {
-                        bufferedReader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        protected void onPostExecute(JSONObject response)
-        {
-            if(response != null)
-            {
-                try {
-                    Log.e("App", "Success: " + response.getString("yourJsonElement") );
-                } catch (JSONException ex) {
-                    Log.e("App", "Failure", ex);
-                }
-            }
-        }
-    }
-
-
-   *//* public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }*//*
-
-    *//*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*//*
-}*/
